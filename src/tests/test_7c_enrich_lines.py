927bc5617aafe6fd6e5b7c8fcf8032922ab6f65a
kyle-woodward <kw.geospatial@gmail.com>
2023-01-25 10:27:00 -0500

    big reorganization

---
+++ src/tests/test_7c_enrich_lines.py
@@ -0,0 +1,10 @@
+#%%
+import arcpy
+from scripts._7c_enrichments_lines import cEnrichmentsLines
+from sys import argv
+from edited.scripts.utils import init_gdb, runner
+import os
+original_gdb, workspace, scratch_workspace = init_gdb()
+line_fc = os.path.join(workspace,'c_Standardized','CM_CNRAExtract_TrtLn_standardized_20221110') # CM_CNRAExtract_TrtLn_standardized_20221110, CalTrans_act_ln_standardized_20220712,
+cEnrichmentsLines(line_fc)
+# # %%g
