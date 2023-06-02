87735f39b76c65cef301d9f1cc3f8b67db9aad0e
unknown <kayla.johnston73@gmail.com>
2023-06-02 14:31:34 -0700

    _6u working

--- src/scripts/utils.py
+++ src/scripts/utils.py
@@ -149,6 +149,40 @@ def og_file_input(prefix:str, filetype:str, gdb):
     most_recent = files_w_prefix[-1]
     return most_recent
 
+def KeepFields(input_table):
+        arcpy.env.overwriteOutput = True
+        fields_kept = arcpy.management.DeleteField(
+                                        in_table=input_table, 
+                                        drop_field=["PROJECTID_USER", "AGENCY", "ORG_ADMIN_p", 
+                                                    "PROJECT_CONTACT", "PROJECT_EMAIL", "ADMINISTERING_ORG", 
+                                                    "PROJECT_NAME", "PROJECT_STATUS", "PROJECT_START", 
+                                                    "PROJECT_END", "PRIMARY_FUNDING_SOURCE", "PRIMARY_FUNDING_ORG", 
+                                                    "IMPLEMENTING_ORG", "LATITUDE", "LONGITUDE", 
+                                                    "BatchID_p", "Val_Status_p", "Val_Message_p", 
+                                                    "Val_RunDate_p", "Review_Status_p", "Review_Message_p", 
+                                                    "Review_RunDate_p", "Dataload_Status_p", "Dataload_Msg_p", 
+                                                    "TRMTID_USER", "PROJECTID", "PROJECTNAME_", 
+                                                    "ORG_ADMIN_t", "PRIMARY_OWNERSHIP_GROUP", "PRIMARY_OBJECTIVE", 
+                                                    "SECONDARY_OBJECTIVE", "TERTIARY_OBJECTIVE", "TREATMENT_STATUS", 
+                                                    "COUNTY", "IN_WUI", "REGION", "TREATMENT_AREA", "TREATMENT_START", 
+                                                    "TREATMENT_END", "RETREATMENT_DATE_EST", "TREATMENT_NAME", "BatchID", 
+                                                    "Val_Status_t", "Val_Message_t", "Val_RunDate_t", "Review_Status_t", 
+                                                    "Review_Message_t", "Review_RunDate_t", "Dataload_Status_t", "Dataload_Msg_t", 
+                                                    "ACTIVID_USER", "TREATMENTID_", "ORG_ADMIN_a", "ACTIVITY_DESCRIPTION", 
+                                                    "ACTIVITY_CAT", "BROAD_VEGETATION_TYPE", "BVT_USERD", "ACTIVITY_STATUS", 
+                                                    "ACTIVITY_QUANTITY", "ACTIVITY_UOM", "ACTIVITY_START", "ACTIVITY_END", 
+                                                    "ADMIN_ORG_NAME", "IMPLEM_ORG_NAME", "PRIMARY_FUND_SRC_NAME", 
+                                                    "PRIMARY_FUND_ORG_NAME", "SECONDARY_FUND_SRC_NAME", "SECONDARY_FUND_ORG_NAME", 
+                                                    "TERTIARY_FUND_SRC_NAME", "TERTIARY_FUND_ORG_NAME", "ACTIVITY_PRCT", 
+                                                    "RESIDUE_FATE", "RESIDUE_FATE_QUANTITY", "RESIDUE_FATE_UNITS", 
+                                                    "ACTIVITY_NAME", "VAL_STATUS_a", "VAL_MSG_a", "VAL_RUNDATE_a", 
+                                                    "REVIEW_STATUS_a", "REVIEW_MSG_a", "REVIEW_RUNDATE_a", "DATALOAD_STATUS_a", 
+                                                    "DATALOAD_MSG_a", "Source", "Year", "Year_txt", "Act_Code", "Crosswalk", 
+                                                    "Federal_FY", "State_FY", "TRMT_GEOM", "COUNTS_TO_MAS"], 
+                                        method="KEEP_FIELDS"
+                                        )
+        return fields_kept
+
 def check_schema_lock(input):
     """
     Checks the schema lock on input files
@@ -177,3 +211,5 @@ def runner(workspace:str,scratch_workspace:str,func, arg):
     transferGDBAttributeProperties=True, 
     workspace=workspace):
         func(arg)
+
+
--- src/tests/test_7a_enrichments_polygon.py
+++ src/tests/test_7a_enrichments_polygon.py
@@ -4,9 +4,13 @@ from scripts._7a_enrichments_polygon import aEnrichmentsPolygon1
 from sys import argv
 from scripts.utils import init_gdb, runner
 import os
+import datetime
 original_gdb, workspace, scratch_workspace = init_gdb()
+
+date_id = datetime.datetime.now().strftime("%Y-%m-%d").replace('-','') # like 20221216
+
 # CalVTP_standardized_20220923="C:\\Users\\sageg\\Documents\\ArcGIS\\Projects\\PC414 CWI Million Acres\\PC414 CWI Million Acres.gdb\\c_Standardized\\CalVTP_standardized_20220923"):  # 7a Enrichments Polygon
-enrich_out = os.path.join(workspace,'d_Enriched','testEnrichments_CalVTP_standardized_20220923')
-enrich_in = os.path.join(workspace,'c_Standardized','CalVTP_standardized_20220923')
+enrich_out = os.path.join(workspace,'d_Enriched',f'_1_TEST_enriched_{date_id}')
+enrich_in = os.path.join(workspace,'c_Standardized','Tahoe_Forest_Fuels_Tx_standardized_20221017')
 aEnrichmentsPolygon1(enrich_out, enrich_in)
 # %%
